15c463c1a63c627e1200e86db77f78bce064f6dd
Florian Kostenzer <florian.kostenzer@icloud.com>
2019-04-16 18:35:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,13 @@
-// swift-tools-version:4.0
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-09-20 19:18:02 +0000
+// swift-tools-version:4.1
+
 import PackageDescription
 let package = Package(
 	name: "PerfectCURL",
 	products: [.library(name: "PerfectCURL", targets: ["PerfectCURL"])],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", from: "2.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", .upToNextMinor(from: "3.0.22")),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", .upToNextMinor(from: "3.0.6")),
 	],
 	targets: [
 		.target(name: "PerfectCURL", dependencies: ["PerfectLib", "PerfectThread"]),
